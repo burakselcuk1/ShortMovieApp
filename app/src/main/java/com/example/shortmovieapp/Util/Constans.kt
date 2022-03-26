69afd5b95965ac2e4d9bb6f0b4101a4a8c38f08b
Burak <design.burakk@gmail.com>
2022-03-26 17:29:17 +0300

    Util class created

---
+++ app/src/main/java/com/example/shortmovieapp/Util/Constans.kt
@@ -0,0 +1,11 @@
+package com.example.shortmovieapp.Util
+
+class Constans {
+    companion object {
+        const  val API_KEY = "6eeb39d6df396b3373f188208038112c"
+        const val BASE_URL ="https://api.themoviedb.org/"
+        const val SEARCH_NEWS_TIME_DELAY = 500L
+        const val QUERY_PAGE_SIZE = 20
+        const val POSTER_MAIN_URL= "https://image.tmdb.org/t/p/w342"
+    }
+}
