aae3b83c1a69ca4c412d6f8b89844a73fbda28a0
Burak <design.burakk@gmail.com>
2022-03-26 23:58:14 +0300

    MovieRepository.kt reviewed

--- app/src/main/java/com/example/shortmovieapp/repository/MovieRepository.kt
+++ app/src/main/java/com/example/shortmovieapp/repository/MovieRepository.kt
@@ -3,7 +3,7 @@ package com.example.shortmovieapp.repository
 import com.example.shortmovieapp.service.Api
 import javax.inject.Inject
 
-class movieRepository @Inject constructor(private val movieApi: Api) {
+class MovieRepository @Inject constructor(private val movieApi: Api) {
 
     suspend fun getMovies()=movieApi.getUpcomingList()
 
