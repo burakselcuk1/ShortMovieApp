51a352c62e0832770fb7ec9af6c8a6c52130f29d
Burak <design.burakk@gmail.com>
2022-03-26 23:57:48 +0300

    Movie.kt reviewed

--- app/src/main/java/com/example/shortmovieapp/model/Movie.kt
+++ app/src/main/java/com/example/shortmovieapp/model/Movie.kt
@@ -2,7 +2,7 @@ package com.example.shortmovieapp.model
 
 import com.example.shortmovieapp.model.Result
 
-data class movie(
+data class Movie(
     val page: Int,
     val results: List<Result>,
     val total_pages: Int,
