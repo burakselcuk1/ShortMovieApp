f2cbdb07a70a05795b94492a7ad7fc667c49bf15
Burak <design.burakk@gmail.com>
2022-03-26 23:56:22 +0300

    Api.kt reviewed

--- app/src/main/java/com/example/shortmovieapp/service/Api.kt
+++ app/src/main/java/com/example/shortmovieapp/service/Api.kt
@@ -2,7 +2,7 @@ package com.example.shortmovieapp.service
 
 import com.example.shortmovieapp.Util.Constans.Companion.API_KEY
 import com.example.shortmovieapp.model.Result
-import com.example.shortmovieapp.model.movie
+import com.example.shortmovieapp.model.Movie
 import retrofit2.Response
 import retrofit2.http.GET
 import retrofit2.http.Path
@@ -14,17 +14,17 @@ interface Api {
     suspend fun getUpcomingList(
         @Query("api_key")
         api_key:String=API_KEY
-    ): Response<movie>
+    ): Response<Movie>
 
     @GET("3/movie/now_playing?")
     suspend fun getNowPlayingList(
         @Query("api_key")
-        api_key:String=API_KEY
-    ): Response<movie>
+        apiKey:String=API_KEY
+    ): Response<Movie>
 
     @GET("3/movie/{movie_id}?")
     suspend fun getMovieDetail(
         @Path("movie_id") id:String,
-        @Query("api_key") api_key: String = API_KEY
+        @Query("api_key") apiKey: String = API_KEY
     ): Response<Result>
 }
