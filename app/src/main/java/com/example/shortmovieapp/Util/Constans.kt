e57b1c0d54775464a59a4de73d8dbc6267792b3b
Burak <design.burakk@gmail.com>
2022-03-27 17:17:28 +0300

    Constans.kt reviewed

--- app/src/main/java/com/example/shortmovieapp/Util/Constans.kt
+++ app/src/main/java/com/example/shortmovieapp/Util/Constans.kt
@@ -4,8 +4,6 @@ class Constans {
     companion object {
         const  val API_KEY = "6eeb39d6df396b3373f188208038112c"
         const val BASE_URL ="https://api.themoviedb.org/"
-        const val SEARCH_NEWS_TIME_DELAY = 500L
-        const val QUERY_PAGE_SIZE = 20
         const val POSTER_MAIN_URL= "https://image.tmdb.org/t/p/w342"
     }
 }
