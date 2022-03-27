696f337a8d6e315b1854d87861e3ae14ca6ec114
Burak <design.burakk@gmail.com>
2022-03-27 15:05:42 +0300

    firebase Autation added

--- app/src/main/java/com/example/shortmovieapp/ui/LoginActivity.kt
+++ app/src/main/java/com/example/shortmovieapp/ui/LoginActivity.kt
@@ -1,12 +1,79 @@
 package com.example.shortmovieapp.ui
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Patterns
+import android.widget.Toast
+import com.example.shortmovieapp.MainActivity
 import com.example.shortmovieapp.R
+import com.example.shortmovieapp.databinding.ActivityLoginBinding
+import com.example.shortmovieapp.databinding.ActivityMainBinding
+import com.google.firebase.auth.FirebaseAuth
+import kotlinx.android.synthetic.main.activity_login.*
 
 class LoginActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityLoginBinding
+    private lateinit var firebaseAuth: FirebaseAuth
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_login)
+        binding = ActivityLoginBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        firebaseAuth = FirebaseAuth.getInstance()
+
+        //  Checks if already logged in
+        checkUser()
+
+        //Goes to register.xml for create new account
+        binding.registerTextview.setOnClickListener {
+            val intent = Intent(this,RegisterActivity::class.java)
+            startActivity(intent)
+        }
+
+        binding.loginButton.setOnClickListener {
+            singIn()
+        }
     }
-}
+
+    // singin into application
+    private fun singIn() {
+
+        val loginemail = binding.loginEmail.text.toString().trim()
+        val loginPassword = binding.loginPassword.text.toString().trim()
+
+        //check e-mail empty or not
+        if(loginemail.isEmpty()){
+            Toast.makeText(this,"E-mail can not be empty!", Toast.LENGTH_SHORT).show()
+        }
+        // compare e-mail adreess with real e-mail type
+        else if(!Patterns.EMAIL_ADDRESS.matcher(loginemail).matches()){
+            Toast.makeText(this,"Invalid E-mail adress!", Toast.LENGTH_SHORT).show()
+
+        }
+        //check password is empty or not
+        else if(loginPassword.isEmpty()){
+            Toast.makeText(this,"Password can not be empty!", Toast.LENGTH_SHORT).show()
+        }
+        else{
+            firebaseAuth.signInWithEmailAndPassword(loginemail,loginPassword).addOnSuccessListener {
+                val intent = Intent(this,MainActivity::class.java)
+                startActivity(intent)
+            }.addOnFailureListener{
+                Toast.makeText(this,"User didn't find!", Toast.LENGTH_SHORT).show()
+            }
+        }
+    }
+
+    private fun checkUser() {
+        val firebase = firebaseAuth.currentUser
+        if (firebase != null){
+            Toast.makeText(this,"Welcome", Toast.LENGTH_SHORT).show()
+            val intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
+        }
+    }
+    }
--- app/src/main/java/com/example/shortmovieapp/ui/RegisterActivity.kt
+++ app/src/main/java/com/example/shortmovieapp/ui/RegisterActivity.kt
@@ -1,12 +1,59 @@
 package com.example.shortmovieapp.ui
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Patterns
+import android.widget.Toast
 import com.example.shortmovieapp.R
+import com.example.shortmovieapp.databinding.ActivityLoginBinding
+import com.example.shortmovieapp.databinding.ActivityRegisterBinding
+import com.google.firebase.auth.FirebaseAuth
+import kotlinx.android.synthetic.main.activity_register.*
 
 class RegisterActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityRegisterBinding
+    private lateinit var firebaseAuth: FirebaseAuth
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_register)
+        binding = ActivityRegisterBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        firebaseAuth = FirebaseAuth.getInstance()
+
+        binding.registerButton.setOnClickListener {
+            createNewUser()
+
+        }
+    }
+
+    private fun createNewUser() {
+        val registerEmail = binding.registerEmail.text.toString().trim()
+        val registerPassword = binding.registerPassword.text.toString().trim()
+
+
+        if(registerEmail.isEmpty()){
+            Toast.makeText(this,"E-mail can not be empty!", Toast.LENGTH_SHORT).show()
+        }
+        else if(!Patterns.EMAIL_ADDRESS.matcher(registerEmail).matches()){
+            Toast.makeText(this,"Invalid E-mail adress!", Toast.LENGTH_SHORT).show()
+
+        }
+        else if(registerPassword.isEmpty()){
+            Toast.makeText(this,"Password can not be empty!", Toast.LENGTH_SHORT).show()
+        }
+        else{
+            firebaseAuth.createUserWithEmailAndPassword(registerEmail,registerPassword).addOnSuccessListener {
+                Toast.makeText(this,"User Created!", Toast.LENGTH_SHORT).show()
+                val intent = Intent(this,LoginActivity::class.java)
+                startActivity(intent)
+            }.addOnFailureListener {
+                Toast.makeText(this,"Error!", Toast.LENGTH_SHORT).show()
+
+            }
+
+        }
     }
 }
