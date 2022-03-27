25d01153ce9df52a0271a9ec9afe4f29e83adfdd
Burak <design.burakk@gmail.com>
2022-03-27 14:43:01 +0300

    SplashActivity.kt added

--- app/src/main/java/com/example/shortmovieapp/MainActivity.kt
+++ app/src/main/java/com/example/shortmovieapp/MainActivity.kt
@@ -1,8 +1,12 @@
 package com.example.shortmovieapp
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
 import android.widget.PopupMenu
+import android.widget.Toast
 import androidx.navigation.NavController
 import androidx.navigation.findNavController
 import androidx.navigation.ui.setupActionBarWithNavController
@@ -21,13 +25,26 @@ class MainActivity : AppCompatActivity() {
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-
         navController = findNavController(R.id.main_fragment)
         setupActionBarWithNavController(navController)
         setupSmoothBottomMenu()
     }
 
+    //Top option menu
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.top_menu, menu)
+        return true
+    }
 
+    //Top option menu click
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when (item.itemId) {
+            R.id.logout -> {
+                Toast.makeText(this,"Logget Out!", Toast.LENGTH_SHORT).show()
+            }
+        }
+        return super.onOptionsItemSelected(item)
+    }
 
     // Bottom navigation menu setup
     private fun setupSmoothBottomMenu() {
@@ -36,6 +53,7 @@ class MainActivity : AppCompatActivity() {
         val menu = popupMenu.menu
         binding.bottomBar.setupWithNavController(menu, navController)
     }
+
     //For back proccess
     override fun onSupportNavigateUp(): Boolean {
         return navController.navigateUp() || super.onSupportNavigateUp()
---
+++ app/src/main/java/com/example/shortmovieapp/ui/SplashActivity.kt
@@ -0,0 +1,23 @@
+package com.example.shortmovieapp.ui
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.os.Handler
+import com.example.shortmovieapp.MainActivity
+import com.example.shortmovieapp.R
+
+class SplashActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash)
+
+        Handler().postDelayed({
+            val intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
+            finish()
+        }, 4000)
+        supportActionBar?.hide()
+
+    }
+}
