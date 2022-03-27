d17a454411437ba83077b717de2383fe0a30e5dc
Burak <design.burakk@gmail.com>
2022-03-27 14:47:20 +0300

    login and RegisterActivity.kt added

---
+++ app/src/main/java/com/example/shortmovieapp/ui/RegisterActivity.kt
@@ -0,0 +1,12 @@
+package com.example.shortmovieapp.ui
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.shortmovieapp.R
+
+class RegisterActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_register)
+    }
+}
--- app/src/main/java/com/example/shortmovieapp/ui/SplashActivity.kt
+++ app/src/main/java/com/example/shortmovieapp/ui/SplashActivity.kt
@@ -13,7 +13,7 @@ class SplashActivity : AppCompatActivity() {
         setContentView(R.layout.activity_splash)
 
         Handler().postDelayed({
-            val intent = Intent(this, MainActivity::class.java)
+            val intent = Intent(this, LoginActivity::class.java)
             startActivity(intent)
             finish()
         }, 4000)
